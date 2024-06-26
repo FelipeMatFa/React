a87c5d0e619a0fd803ff609f6eed1c3d7bafd3c6
FelipeMatFa <Felipe2006mattosdefarias@gmail.com>
2024-06-26 17:25:20 -0300

    React-com-componente

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import '../style.css'
+
+function main({name}){
+    return(
+        <div className='div_mensagem'>
+            <h1>Olá, {name}!</h1>
+        </div>
+    )
+}
+
+export default main;
